848796f49041415346c98bd1280f0489b79c5f78
mitalineer <mitalineer@gmail.com>
2022-12-27 00:02:43 +0530

    checking if the image rendering works

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className="container">
       <head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
-        />
+        /> */}
       </head>
       <h1>Pari Home Decore Furniture</h1>
       <div class="btn-group">
@@ -18,8 +18,8 @@ export default function Home() {
         <button>Mattress</button>
       </div>
       <p> For More Information Contact : neernpatel@gmail.com </p>
-      <img src="public/netliheart.svg"> </img>
-      <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+      {/* <img src="public/netliheart.svg"> </img> */}
+      {/* <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script> */}
     </div>
   )
 }
