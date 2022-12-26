033c0e2215f7acc38ca6c2d6021907f65f6f6747
mitalineer <mitalineer@gmail.com>
2022-12-26 10:07:39 +0530

    removed chagnes from index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Pari Home Decore!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
