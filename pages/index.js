88eb3dbbaf402da02de179c74ba53b9e4c5fb584
mitalineer <mitalineer@gmail.com>
2022-12-26 10:05:50 +0530

    removed chagnes from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
 export default function Home() {
   return (
     <div className="container">
@@ -11,7 +10,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Pari Home Decore!"" />
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
       </main>
 
       <Footer />
